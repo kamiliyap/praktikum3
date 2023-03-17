076f445a64342b6ae23b5c07d9da039017a352c8
kamiliyap <92785222+kamiliyap@users.noreply.github.com>
2023-03-17 12:03:05 +0700

    Add files via upload

---
+++ Pertemuan3 pw2/tugas/home.php
@@ -0,0 +1,16 @@
+<?php
+
+	include_once "header.php"
+
+?>
+
+				<h1>Welcome Home!</h1>
+				<p>
+                    Kamiliya Latifah Prasmaisya - 0110221303 - TI13
+				</p>
+				
+<?php
+
+include_once "footer.php"
+
+?>
